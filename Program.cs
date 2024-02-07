e28fc5ad0de512814da1c003c08e61b843642e8e
GabrielaKoleva <gabriela.t.koleva@gmail.com>
2024-02-07 21:36:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+string town = Console.ReadLine();
+string contry = Console.ReadLine();
+Console.WriteLine(town + " is in " + contry);
