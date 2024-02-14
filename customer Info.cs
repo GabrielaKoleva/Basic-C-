758ba0e6eccbc5bc65a31e2bf6a2b59f4fcd0035
GabrielaKoleva <gabriela.t.koleva@gmail.com>
2024-02-14 22:06:32 +0200

    Update Customer Info.cs
    Write a program that:
    Reads customer's first name, last name and email
    Prints the read data in the following format: "Customer: {first name} {last name} ({email})"

--- customer Info.cs
+++ customer Info.cs
@@ -1,9 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
 string firstName = Console.ReadLine();
 string LastName = Console.ReadLine();
 string email = Console.ReadLine();
-Console.WriteLine("Customer: "+ firstName + " " 
-    + LastName + " " + "(" + email +")");
 
-Console.WriteLine($"Customer: {firstName} " +
-    $"{LastName} ({email}");
+Console.WriteLine($"Customer: {firstName} + {LastName} ({email}");
