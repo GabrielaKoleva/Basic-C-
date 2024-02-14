1f4c79d4b76d35ca9baf7be2a25701df53d73271
GabrielaKoleva <gabriela.t.koleva@gmail.com>
2024-02-14 21:59:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string firstName = Console.ReadLine();
+string LastName = Console.ReadLine();
+string email = Console.ReadLine();
+Console.WriteLine("Customer: "+ firstName + " " 
+    + LastName + " " + "(" + email +")");
+
+Console.WriteLine($"Customer: {firstName} " +
+    $"{LastName} ({email}");
