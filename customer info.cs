65e20e31f5f1d02dc28ff52910ff79e1055a068d
GabrielaKoleva <gabriela.t.koleva@gmail.com>
2024-02-14 22:08:06 +0200

    Delete customer info.cs

--- customer info.cs
+++
@@ -1,8 +0,0 @@
-string firstName = Console.ReadLine();
-string LastName = Console.ReadLine();
-string email = Console.ReadLine();
-Console.WriteLine("Customer: "+ firstName + " " 
-    + LastName + " " + "(" + email +")");
-
-Console.WriteLine($"Customer: {firstName} " +
-    $"{LastName} ({email}");
