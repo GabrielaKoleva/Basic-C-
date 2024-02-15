0556b05b7e63eecb5287d985f4d40b2117511a93
GabrielaKoleva <gabriela.t.koleva@gmail.com>
2024-02-15 23:20:43 +0200

    Add files via upload
    Write a program to convert days to minutes:
    Read a single integer (the days to be converted)
    Convert the days to minutes (1 day = 24 hours * 60 minutes)
    Print the minutes in the following format: "Minutes = {calculated minutes}"

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int days = int.Parse(Console.ReadLine());
+
+int convert = (days * 24) * 60;
+
+Console.WriteLine("Minutes = " + convert);
