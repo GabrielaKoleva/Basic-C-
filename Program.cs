6751e0475b03d190f6bc783e7e2b596f5444cc5a
GabrielaKoleva <gabriela.t.koleva@gmail.com>
2024-02-15 23:18:28 +0200

    Add files via upload
    Write a console program that:
    Reads three integer numbers, which represents first base, second base and height of the trapezoid
    Calculates trapezoid's area 
    Prints the calculated area on the console
    
    Note: Trapezoid's area is calculated when you sum two bases, divide them by two and the result is multiplied by height: (first base + second base) / 2 * height

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int firstSide = int.Parse(Console.ReadLine());
+int secondSide = int.Parse(Console.ReadLine());
+int hight = int.Parse(Console.ReadLine());
+
+int area = (firstSide + secondSide) / 2 * hight;
+
+Console.WriteLine(area);
+
