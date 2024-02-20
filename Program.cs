2f40e7d8c8433b6a082ce1b3cae7d42ef6d4fb8d
GabrielaKoleva <gabriela.t.koleva@gmail.com>
2024-02-20 21:34:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+double tomatoPrice = double.Parse(Console.ReadLine());
+double tomatoQuantity = double.Parse(Console.ReadLine());
+double cucumberPrice = double.Parse(Console.ReadLine());
+double cucumberQuantity = double.Parse(Console.ReadLine());
+
+double allTomato = tomatoPrice * tomatoQuantity;
+double allCucumber = cucumberPrice * cucumberQuantity;
+
+double total = allTomato + allCucumber;
+
+Console.WriteLine($"{total:F2}");
