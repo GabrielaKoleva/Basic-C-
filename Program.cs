c2092313935f7b1582e33c6340c2ca9685de8bfe
GabrielaKoleva <gabriela.t.koleva@gmail.com>
2024-02-20 21:32:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int dogFood = int.Parse(Console.ReadLine());
+int catFood = int.Parse(Console.ReadLine());
+
+double priceDogFood = dogFood * 2.50;
+double priceCatFood = catFood * 4;
+
+double allPrice = priceDogFood + priceCatFood;
+
+Console.WriteLine($"{allPrice:F2} lv.");
