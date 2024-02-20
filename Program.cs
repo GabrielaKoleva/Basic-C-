b63a1502ea2d3e7409193d0f9d9ad515e0d64430
GabrielaKoleva <gabriela.t.koleva@gmail.com>
2024-02-20 21:33:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+double amount = double.Parse(Console.ReadLine());
+int months = int.Parse(Console.ReadLine());
+double annualInterestRate = double.Parse(Console.ReadLine());
+
+double rateForMonth = (amount * (annualInterestRate / 100)) / 12;
+double totalAmmount = amount + months * rateForMonth;
+
+Console.WriteLine($"{totalAmmount}");
+
